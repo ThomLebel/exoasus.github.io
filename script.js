7476c3ee7941adf38880733e70a854e0d1bec573
Thomas Lebel <thom.lebel@orange.fr>
2022-05-10 15:36:34 +0200

    added bonus exo

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let cta_video1, cta_video2, video_overlay, video_player, close_btn, img_podium, img_pc, cta_podium, bubble;
-let videoIDs = ["0sjUjl_7nI8", "zIgEaGz_CO4"]
+let cta_wtb = [];
+let highlightedWTB = 0;
+let highlightInterval;
+let videoIDs = ["0sjUjl_7nI8", "zIgEaGz_CO4"];
+let wtb_links = [
+    "https://www.ldlc.com/fiche/PB00469315.html",
+    "https://estore.asus.com/fr/90nb0vc2-m005a0-vivobook-13-slate-oled-t3300.html",
+    "https://www.fnac.com/Pack-PC-portable-2-en-1-Asus-VivoBook-13-Slate-OLED-T3300KA-LQ069W-13-3-Full-HD-tactile-Intel-Pentium-8-Go-RAM-128-Go-SSD-Noir-Housse-stylet/a16314972/w-4#omnsearchpos=1",
+    "https://www.darty.com/nav/achat/informatique/ordinateur_portable-portable/portable/asus_t3300ka-pen_8_28_1.html"
+];
 
 
 function init(){
@@ -11,9 +20,17 @@ function init(){
     img_podium = document.getElementById("img_podium");
     cta_podium = document.getElementById("cta_podium");
     bubble = document.getElementById("bubble");
+    cta_wtb = document.getElementsByClassName("WTB");
 
     cta_video1.dataset.videoid = videoIDs[0];
     cta_video2.dataset.videoid = videoIDs[1];
+
+    for(let i=0; i<cta_wtb.length; i++){
+        cta_wtb[i].addEventListener("click", function(){
+            console.log("test");
+            window.open(wtb_links[i], "_blank");
+        });
+    }
 
     cta_video1.onclick = openVideo;
     cta_video2.onclick = openVideo;
@@ -32,6 +49,8 @@ function init(){
     tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    highlightInterval = setInterval(highlightWTB, 3000);
 }
 
 function onYouTubeIframeAPIReady() {
@@ -65,6 +84,14 @@ function leavePodium(){
     bubble.classList.remove("show");
 }
 
+function highlightWTB(){
+    cta_wtb[highlightedWTB].classList.remove("highlight");
+    highlightedWTB ++;
+
+    if(highlightedWTB >= cta_wtb.length) highlightedWTB = 0;
+
+    cta_wtb[highlightedWTB].classList.add("highlight");
+}
 
 
 
